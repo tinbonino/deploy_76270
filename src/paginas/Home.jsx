43a96e14995001e1b92ae4d196cf851439edb148
martin <tinbonino@gmail.com>
2024-12-02 19:54:07 -0300

    quitando la env

--- src/paginas/Home.jsx
+++ src/paginas/Home.jsx
@@ -3,7 +3,7 @@ function Home() {
   return (
     <>
         <h1>Bienvenidos a la clase 9</h1>
-        <h2>{import.meta.env.MENSAJE}</h2>
+        <h2>hola</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero recusandae sed, eius quod illo eligendi!</p>
     </>
   )
