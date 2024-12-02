f38cb3efa6448a3e74b63007e7745a34020f75ec
martin <tinbonino@gmail.com>
2024-12-02 19:57:15 -0300

    corrigiendo la env

--- src/paginas/Home.jsx
+++ src/paginas/Home.jsx
@@ -4,6 +4,7 @@ function Home() {
     <>
         <h1>Bienvenidos a la clase 9</h1>
         <h2>hola</h2>
+        <h2>{import.meta.env.VITE_MENSAJE}</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero recusandae sed, eius quod illo eligendi!</p>
     </>
   )
